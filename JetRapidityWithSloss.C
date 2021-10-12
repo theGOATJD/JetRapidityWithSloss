e1ffe0ca9e5a45611430c37459b750b345a14766
theGOATJD <48004027+theGOATJD@users.noreply.github.com>
2021-10-12 14:44:32 -0400

    Updated Version With Second Part of JetsPbPb010(1.2-2.1)
    I took the second part of PbPb jets data (1.2-.2.1 in rapidity and 0-10% for centrality class) in the codes.

--- JetRapidityWithSloss.C
+++ JetRapidityWithSloss.C
@@ -58,10 +58,10 @@ c1 = new TCanvas("c1","Fitting",5000,2000);
     
       
      //Get the parameters from previous fitting
-  Float_t a=f1->GetParameter("A");
-  Float_t b=f1->GetParameter("B");
-   Float_t c=f2->GetParameter("C");
-   Float_t d=f2->GetParameter("D");
+  Double_t a=f1->GetParameter("A");
+  Double_t b=f1->GetParameter("B");
+   Double_t c=f2->GetParameter("C");
+   Double_t d=f2->GetParameter("D");
    // Get PbPb data from the following file
    s1= new TGraph("C:/root/JetsPbPb010.txt");
    s2=new TGraph("C:/root/JetsPbPb0102ndPart.txt");
@@ -69,14 +69,16 @@ c1 = new TCanvas("c1","Fitting",5000,2000);
    sr1= new TGraph();
    Int_t j;
     Int_t i;
-
+    
+    
    //There are 6 data points from the PbPb data
     for(j=0;j<6;j++){
      //  say that the transverse momemtum for PbPb data is Pbx
-     Float_t Pbx=s1->GetX()[j];
+     Double_t Pbx=s1->GetX()[j];
      //say that whatever value on the y axis for PbPb data is Pby
-     Float_t Pby=s1->GetY()[j];
-     Float_t PPx=pow((Pby)/a,1/b);
+     Double_t Pby=s1->GetY()[j];
+    
+     Double_t PPx=pow((Pby)/a,1/b);
     
      //print out the corresponding transeverse momentum and the Sloss
      printf("%lf\t%lf\n",PPx,(PPx-Pbx)/PPx);
@@ -88,10 +90,10 @@ c1 = new TCanvas("c1","Fitting",5000,2000);
     
      for(i=0;i<2;i++){
      //  say that the transverse momemtum for PbPb data is Pbx
-     Float_t Pbxx=s2->GetX()[i];
+     Double_t Pbxx=s2->GetX()[i];
      //say that whatever value on the y axis for PbPb data is Pby
-     Float_t Pbyy=s2->GetY()[i];
-     Float_t PPxx=pow((Pbyy)/c,1/d);
+     Double_t Pbyy=s2->GetY()[i];
+     Double_t PPxx=pow((Pbyy)/c,1/d);
      
      //print out the corresponding transeverse momentum and the Sloss
      printf("%lf\t%lf\n",PPxx,(PPxx-Pbxx)/PPxx);
@@ -109,6 +111,7 @@ sr1->SetMarkerStyle(20);
  sr1->SetFillStyle(0);
  sr1->SetMinimum(0);
    sr1->SetMaximum(0.5);
+   
    sr1->Draw("AP");
    
  c2->BuildLegend()->SetTextSize(0.05);
@@ -171,10 +174,10 @@ sr1->SetMarkerStyle(20);
     
       
      //Get the parameters from previous fitting
-  Float_t a=f3->GetParameter("A");
-  Float_t b=f3->GetParameter("B");
-   Float_t c=f4->GetParameter("C");
-   Float_t d=f4->GetParameter("D");
+  Double_t a=f3->GetParameter("A");
+  Double_t b=f3->GetParameter("B");
+   Double_t c=f4->GetParameter("C");
+   Double_t d=f4->GetParameter("D");
    // Get PbPb data from the following file
    s3= new TGraph("C:/root/JetsPbPb010(0.3-0.8).txt");
    s4=new TGraph("C:/root/JetsPbPb010(0.3-0.8)2ndPart.txt");
@@ -186,10 +189,10 @@ sr1->SetMarkerStyle(20);
    //There are 6 data points from the PbPb data
     for(j=0;j<6;j++){
      //  say that the transverse momemtum for PbPb data is Pbx
-     Float_t Pbx=s3->GetX()[j];
+     Double_t Pbx=s3->GetX()[j];
      //say that whatever value on the y axis for PbPb data is Pby
-     Float_t Pby=s3->GetY()[j];
-     Float_t PPx=pow((Pby)/a,1/b);
+     Double_t Pby=s3->GetY()[j];
+     Double_t PPx=pow((Pby)/a,1/b);
     
      //print out the corresponding transeverse momentum and the Sloss
      printf("%lf\t%lf\n",PPx,(PPx-Pbx)/PPx);
@@ -201,10 +204,10 @@ sr1->SetMarkerStyle(20);
     
      for(i=0;i<3;i++){
      //  say that the transverse momemtum for PbPb data is Pbx
-     Float_t Pbxx=s4->GetX()[i];
+     Double_t Pbxx=s4->GetX()[i];
      //say that whatever value on the y axis for PbPb data is Pby
-     Float_t Pbyy=s4->GetY()[i];
-     Float_t PPxx=pow((Pbyy)/c,1/d);
+     Double_t Pbyy=s4->GetY()[i];
+     Double_t PPxx=pow((Pbyy)/c,1/d);
      
      //print out the corresponding transeverse momentum and the Sloss
      printf("%lf\t%lf\n",PPxx,(PPxx-Pbxx)/PPxx);
@@ -282,10 +285,10 @@ sr2->SetMarkerStyle(20);
     
       
      //Get the parameters from previous fitting
-  Float_t a=f5->GetParameter("A");
-  Float_t b=f5->GetParameter("B");
-   Float_t c=f6->GetParameter("C");
-   Float_t d=f6->GetParameter("D");
+  Double_t a=f5->GetParameter("A");
+  Double_t b=f5->GetParameter("B");
+   Double_t c=f6->GetParameter("C");
+   Double_t d=f6->GetParameter("D");
    // Get PbPb data from the following file
    s5= new TGraph("C:/root/JetsPbPb010(0.8-1.2).txt");
    s6=new TGraph("C:/root/JetsPbPb010(0.8-1.2)2ndPart.txt");
@@ -297,10 +300,10 @@ sr2->SetMarkerStyle(20);
    //There are 6 data points from the PbPb data
     for(j=0;j<6;j++){
      //  say that the transverse momemtum for PbPb data is Pbx
-     Float_t Pbx=s5->GetX()[j];
+     Double_t Pbx=s5->GetX()[j];
      //say that whatever value on the y axis for PbPb data is Pby
-     Float_t Pby=s5->GetY()[j];
-     Float_t PPx=pow((Pby)/a,1/b);
+     Double_t Pby=s5->GetY()[j];
+     Double_t PPx=pow((Pby)/a,1/b);
     
      //print out the corresponding transeverse momentum and the Sloss
      printf("%lf\t%lf\n",PPx,(PPx-Pbx)/PPx);
@@ -312,15 +315,15 @@ sr2->SetMarkerStyle(20);
     
      for(i=0;i<3;i++){
      //  say that the transverse momemtum for PbPb data is Pbx
-     Float_t Pbxx=s6->GetX()[i];
+     Double_t Pbxx=s6->GetX()[i];
      //say that whatever value on the y axis for PbPb data is Pby
-     Float_t Pbyy=s6->GetY()[i];
-     Float_t PPxx=pow((Pbyy)/c,1/d);
+     Double_t Pbyy=s6->GetY()[i];
+     Double_t PPxx=pow((Pbyy)/c,1/d);
      
      //print out the corresponding transeverse momentum and the Sloss
      printf("%lf\t%lf\n",PPxx,(PPxx-Pbxx)/PPxx);
      
-     // Input all the Sloss value to the graph sr1
+     // Input all the Sloss value to the graph sr3
     
      sr3->SetPoint(i,PPxx,(PPxx-Pbxx)/PPxx);
      
@@ -351,14 +354,26 @@ sr3->SetMarkerStyle(20);
       g4->SetMarkerColor(kRed);
 
       //Fitting
-    TF1* f7= new TF1("f7", "[0]*x^[1]",5,100);
+    TF1* f7= new TF1("f7", "[0]*x^[1]",50,179);
+    TF1* f8= new TF1("f8", "[0]*x^[1]",177,500);
+    TF1* total4= new TF1("total4","[0]*x^[1]+[0]*x^[1]",50,500);
   f7->SetParNames("A","B");
   f7->SetParameters(  7.18296e+006, -5.77399e+000);
   f7->SetLineColor(kBlack);
  
    f7->Draw("same");
     f7->SetTitle("Fitting");
-   g4->Fit(f7);
+
+    f8->SetParNames("C","D");
+  f8->SetParameters(  7.18296e+006, -5.77399e+000);
+  f8->SetLineColor(kBlue);
+ 
+   f8->Draw("same");
+    f8->SetTitle("Fitting2");
+
+     g4->Fit(f7,"R");
+    g4->Fit(f8,"R+");
+    g4->Fit(total4,"R+");
  c7->BuildLegend();
     c7.SetLogy();
 
@@ -375,27 +390,44 @@ sr3->SetMarkerStyle(20);
      
       
      //Get the parameters from previous fitting
-  Float_t a=f7->GetParameter("A");
-  Float_t b=f7->GetParameter("B");
- 
+  Double_t a=f7->GetParameter("A");
+  Double_t b=f7->GetParameter("B");
+  Double_t c=f8->GetParameter("C");
+  Double_t d=f8->GetParameter("D");
   //Get PbPb data from the following file
    s7= new TGraph("C:/root/JetsPbPb010(1.2-2.1).txt");
-   
+   s8=new TGraph("C:/root/JetsPbPb010(1.2-2.1)2ndPart.txt");
    //Create a new graph called sr1 for plotting the Sloss value
    sr4= new TGraph();
    Int_t j;
-   
+   Int_t i;
 
    //There are 6 data points from the PbPb data
    for(j=0;j<6;j++){
      //  say that the transverse momemtum for PbPb data is Pbx
-     Float_t Pbx=s7->GetX()[j];
+     Double_t Pbx=s7->GetX()[j];
      //say that whatever value on the y axis for PbPb data is Pby
-     Float_t Pby=s7->GetY()[j];
-     Float_t PPx=pow((Pby)/a,1/b);
+     Double_t Pby=s7->GetY()[j];
+     Double_t PPx=pow((Pby)/a,1/b);
      //print out the corresponding transeverse momentum and the Sloss
      printf("%lf\t%lf\n",PPx,(PPx-Pbx)/PPx);
      sr4->SetPoint(j,PPx,(PPx-Pbx)/PPx);
+   }
+   
+   for(i=0;i<2;i++){
+     //  say that the transverse momemtum for PbPb data is Pbx
+     Double_t Pbxx=s8->GetX()[i];
+     //say that whatever value on the y axis for PbPb data is Pby
+     Double_t Pbyy=s8->GetY()[i];
+     Double_t PPxx=pow((Pbyy)/c,1/d);
+     
+     //print out the corresponding transeverse momentum and the Sloss
+     printf("%lf\t%lf\n",PPxx,(PPxx-Pbxx)/PPxx);
+     
+     // Input all the Sloss value to the graph sr4
+    
+     sr4->SetPoint(i,PPxx,(PPxx-Pbxx)/PPxx);
+     
    }
    //No second part calculation for rapdity ranges from 1.2-2.1
    //because one Pbx value which is one of the y value from PbPb Spectra is zero   //Based on Sloss equation, plugging zero would make Sloss undefined.
